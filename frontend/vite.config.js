d320af7ba7a68f91f45492e439e2475f5241a687
Bryan Christy <bryanchristy2016@gmail.com>
2024-08-11 20:57:04 +0530

    CONFIG: Setup vite proxy for backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     port: 2323,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4173',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   plugins: [react()],
 })
